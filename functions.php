b9499515b24c7a5093ffbc22813f66146e5dae32
Volatiana Estelle <estelleherimanana4@gmail.com>
2026-08-21 23:22:56 +0300

    modification-style

--- functions.php
+++ functions.php
@@ -42,3 +42,19 @@ add_action(
     'wp_enqueue_scripts',
     'ankizy_generation_enqueue_assets'
 );
+
+function ankizy_generation_menu_link_attributes($atts, $item, $args) {
+
+    if ($args->theme_location === 'primary') {
+        $atts['class'] = 'main-nav__link';
+    }
+
+    return $atts;
+}
+
+add_filter(
+    'nav_menu_link_attributes',
+    'ankizy_generation_menu_link_attributes',
+    10,
+    3
+);
